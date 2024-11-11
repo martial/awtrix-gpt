2a7a6527ccad40efa59856d6b06310cfc27c2670
Martial <martial@screen-club.com>
2024-11-11 09:56:09 +0100

    upside down

--- managers/printer_manager.py
+++ managers/printer_manager.py
@@ -65,7 +65,7 @@ class ThermalPrinterManager:
         """Print text"""
         try:    
             
-            self.printer.out(text, strike=True, underline=2,  bold=True)
+            self.printer.out(text, strike=True, underline=2, upside_down=True, bold=True)
             self.printer.feed(feed)
             return True
         except Exception as e:
